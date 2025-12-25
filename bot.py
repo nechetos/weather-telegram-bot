220c6b0c40584c0092b1e243d982d5a964a4863b
nechetos <157478096+nechetos@users.noreply.github.com>
2025-12-25 13:18:16 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,51 @@
-from telegram.ext import Updater, CommandHandler
-import os
+import asyncio
+import requests
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "8165242190:AAGGwSR8oh0oBdtJkIkglIbtAmDBlZG2qfE"
 
-def start(update, context):
-    update.message.reply_text("Привет! Я погодный бот ☀️")
+WEATHER_API_KEY = "f3cc9a774ce15cad663ced259bf571af"
+CITY = "Yekaterinburg"
 
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
-    dp.add_handler(CommandHandler("start", start))
 
-    updater.start_polling()
-    updater.idle()
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(
+        "Привет! Я бот погоды ☁️\n"
+        "Напиши /weather — пришлю прогноз."
+    )
+
+
+@dp.message(Command("weather"))
+async def weather(message: types.Message):
+    url = (
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?q={CITY}&appid={WEATHER_API_KEY}&units=metric&lang=ru"
+    )
+
+    r = requests.get(url)
+    data = r.json()
+
+    if r.status_code != 200:
+        await message.answer("Не удалось получить погоду 😢")
+        return
+
+    temp = data["main"]["temp"]
+    desc = data["weather"][0]["description"]
+
+    await message.answer(
+        f"🌡 Температура: {temp}°C\n"
+        f"☁️ Погода: {desc}"
+    )
+
+
+async def main():
+    await dp.start_polling(bot)
+
 
 if name == "main":
-
-    main()
+    asyncio.run(main())
